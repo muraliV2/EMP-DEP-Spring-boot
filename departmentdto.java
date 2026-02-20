3926eac3a6b3279d7515e88b1d2b923a48e0201c
Murali <muralisurya2004@gmail.com>
2026-02-20 23:46:58 +0530

    Create departmentdto.java

---
+++ departmentdto.java
@@ -0,0 +1,12 @@
+package com.jspiders.dto;
+
+import lombok.Data;
+
+@Data
+public class departmentdto 
+{
+	
+	private int departmentId;
+	private String departmentName;
+	private String location;
+}
