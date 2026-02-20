ce96f6fd2ca2061c98ea69f29cecd2d4263a5fff
Murali <muralisurya2004@gmail.com>
2026-02-21 00:33:02 +0530

    Update departmentrepository.java

--- departmentrepository.java
+++ departmentrepository.java
@@ -1,10 +1,14 @@
 package com.jspiders.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jspiders.Entity.department;
 @Repository
 public interface departmentrepository extends JpaRepository<department,Integer>{
+
+	Optional<department> findByDepartmentName(String departmentName);
 
 }
