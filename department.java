4b16b0a899b5581e1bbe16cab7dde44b4d82b1c1
Murali <muralisurya2004@gmail.com>
2026-02-20 23:47:22 +0530

    Create department.java

---
+++ department.java
@@ -0,0 +1,32 @@
+package com.jspiders.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Data;
+
+@Entity
+@Data
+public class department 
+{
+	@Id
+private int departmentId;
+private String departmentName;
+private String location;
+@OneToMany(mappedBy = "department")
+private List<Employee> listofemployees = new ArrayList<Employee>();
+
+private void addEmployee(Employee employee)
+{
+	listofemployees.add(employee);
+employee.setDepartment(this);
+}
+private void removerEmployee(Employee employee)
+{
+	listofemployees.remove(employee);
+	employee.setDepartment(null);
+}
+}
