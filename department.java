e146d3c4e02ea94d0dfed5a71aedd3b00d0c8853
Murali <muralisurya2004@gmail.com>
2026-02-23 23:52:32 +0530

    Update department.java

--- department.java
+++ department.java
@@ -2,7 +2,6 @@ package com.jspiders.Entity;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
