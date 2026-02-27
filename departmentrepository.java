8c5d7ff7d0cba84839651008914a83f58caf5fdd
Murali <muralisurya2004@gmail.com>
2026-02-27 23:26:05 +0530

    Update departmentrepository.java

--- departmentrepository.java
+++ departmentrepository.java
@@ -3,7 +3,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.jspiders.Entity.department;
 @Repository
 public interface departmentrepository extends JpaRepository<department,Integer>{
