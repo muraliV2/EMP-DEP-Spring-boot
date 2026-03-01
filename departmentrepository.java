26a40a04ed861329c2fa85ea8e07c9376394d4cf
Murali <muralisurya2004@gmail.com>
2026-03-01 20:27:29 +0530

    Update departmentrepository.java

--- departmentrepository.java
+++ departmentrepository.java
@@ -1,6 +1,5 @@
 package com.jspiders.repository;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.jspiders.Entity.department;
