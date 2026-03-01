eb7a1c1d9712a218043143b43b028b46253b121e
Murali <muralisurya2004@gmail.com>
2026-03-01 20:26:57 +0530

    Update mainmethod.java

--- mainmethod.java
+++ mainmethod.java
@@ -1,6 +1,5 @@
 package com.jspiders;
 
-
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
