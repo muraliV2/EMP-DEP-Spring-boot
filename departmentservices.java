5a1658688013ddc678762d61a55bd667f146d455
Murali <muralisurya2004@gmail.com>
2026-03-15 17:38:12 +0530

    Clean up formatting in departmentservices.java
    Removed unnecessary blank lines before the deletedepartment method.

--- departmentservices.java
+++ departmentservices.java
@@ -39,8 +39,6 @@ return structure;
 		return structure;
 	}
 
-
-
 	public ResponseStructure<String> deletedepartment(int departmentId) {
 		String message = departmentdao.deletedepartment(departmentId);
 		ResponseStructure<String> structure = new ResponseStructure<String>();
