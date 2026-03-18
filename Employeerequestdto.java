ca1fe7908c22de0862f1c5c3168dd0d0dab6897b
Murali <muralisurya2004@gmail.com>
2026-03-19 00:55:38 +0530

    Create Employeerequestdto.java

---
+++ Employeerequestdto.java
@@ -0,0 +1,16 @@
+package com.jspiders.dto;
+
+import lombok.Data;
+
+@Data
+public class EmployeeRequestdto 
+{
+	
+    private String employeeName;
+    private String employeeEmail;
+    private long contact;
+    private int salary;
+    private String password;
+    private String designation;
+    private String departmentName; 
+}
