a61a36ee6d9177de96459f46d9e67c8c8ed933ad
Murali <muralisurya2004@gmail.com>
2026-03-19 00:55:59 +0530

    Create Employeeresponsedto.java

---
+++ Employeeresponsedto.java
@@ -0,0 +1,25 @@
+package com.jspiders.dto;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class Employeeresponsedto 
+{
+	
+		
+		private int Employeeid;
+		private String employeeName;
+		private String employeeEmail;
+		private long contact;
+		private int salary;
+		private String designation;
+		private String departmeantName;
+		
+	
+	
+
+}
