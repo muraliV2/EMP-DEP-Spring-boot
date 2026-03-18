016b58bc127704fbd54707f09fb4fcdc50d23c30
Murali <muralisurya2004@gmail.com>
2026-03-19 00:52:01 +0530

    Create employee.java

---
+++ employee.java
@@ -0,0 +1,39 @@
+package com.jspiders.Entity;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.Data;
+
+@Entity
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int employeeid;
+
+    private String employeeName;
+    private String employeeEmail;
+    private String password;
+    private long contact;
+    private int salary;
+    private String designation;
+
+    private LocalDateTime datatime;
+    private boolean accountstatus;
+
+    @ManyToOne
+    private department department;
+
+
+
+    public void addEmployees(department depart) {
+        this.department = depart;
+        depart.getListofemployees().add(this);
+    }
+}
