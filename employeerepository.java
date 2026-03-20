a676af634d7e2e5b4f2064641ea5fd3f4e028219
Murali <muralisurya2004@gmail.com>
2026-03-21 00:12:21 +0530

    Create employeerepository.java

---
+++ employeerepository.java
@@ -0,0 +1,16 @@
+package com.jspiders.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jspiders.Entity.Employee;
+
+public interface Employeerepository extends JpaRepository<Employee, Integer>
+{
+
+	List<Employee> findByDepartment_DepartmentName(String departmentName);
+
+	List<Employee> findByDepartment_DepartmentId(int departmentId);
+
+}
