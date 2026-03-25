e1cd17a041684d92640dec91e171117ee9af9be1
Murali <muralisurya2004@gmail.com>
2026-03-25 23:25:53 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -1,6 +1,5 @@
 package com.jspiders.Entity;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
