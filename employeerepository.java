f151e43420e246757f1657d1fcf85708f46e4b66
Murali <muralisurya2004@gmail.com>
2026-07-08 23:19:40 +0530

    Update employeerepository.java

--- employeerepository.java
+++ employeerepository.java
@@ -7,7 +7,6 @@ import com.jspiders.Entity.Employee;
 public interface Employeerepository extends JpaRepository<Employee, Integer>
 {
 List<Employee> findByDepartment_DepartmentName(String departmentName);
-
 List<Employee> findByDepartment_DepartmentId(int departmentId);
 
 }
