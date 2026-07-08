b58df7ccd2e57d990ff05eb732997acba7d4b850
Murali <muralisurya2004@gmail.com>
2026-07-08 23:19:08 +0530

    Update employeerepository.java

--- employeerepository.java
+++ employeerepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jspiders.Entity.Employee;
-
 public interface Employeerepository extends JpaRepository<Employee, Integer>
 {
 
